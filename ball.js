4696de04563cc22b64768acb4b8a4b8bb23e9784
Autumn <76168014+Autumn64@users.noreply.github.com>
2023-04-14 17:56:13 -0600

    Added the option to save and modify the username

--- ball.js
+++ ball.js
@@ -1,13 +1,28 @@
 let person;
+let user = document.cookie;
 
 function getperson(){
     try{
-        do{
-            person = prompt("Please enter a username.");
-        }while(person.trim() === "" || person === null);
+        if(user != ""){
+            person = user.split(";")[0].substring(9)
+        }else{
+            do{
+                person = prompt("Please enter a username.");
+            }while(person.trim() === "" || person === null);
+            var expiration_date = new Date();
+            expiration_date.setYear (expiration_date.getFullYear () + 1);
+            expiration_date = expiration_date.toGMTString();
+            var cookie_string = "username=" + person + "; path=/; expires=" + expiration_date;
+            document.cookie = cookie_string;
+        }
     }catch(e){
         getperson();
     }
+}
+
+function changeUser(){
+    user=""
+    getperson();
 }
 
 function doFunction(){
