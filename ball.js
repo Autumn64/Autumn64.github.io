2f64fb530d5bfc353b091a2f447f38b0dd5ac5e8
Autumn <76168014+Autumn64@users.noreply.github.com>
2023-05-27 23:57:31 -0600

    Now chat history is saved!

--- ball.js
+++ ball.js
@@ -26,9 +26,41 @@ function getperson(){
     }
 }
 
-function changeUser(){
-    user=""
-    getperson();
+function saveMessage(message){ //Save the chat history
+    var chath = localStorage.getItem("chatHistory");
+    var chata = chath ? JSON.parse(chath) : []; //Checks if chath returned null, if so, it creates a blank array; if not, it parses it to JSON.
+    chata.push(message);
+    if(chata.length > 50){ //We can't store the whole goddamn chat history in the local storage
+        chata.shift(); //Delete the oldest message
+    }
+    localStorage.setItem("chatHistory", JSON.stringify(chata)); //Saves the array to the local storage with the key "chatHistory"
+}
+
+function displayHistory(){
+    var chath = localStorage.getItem("chatHistory");
+    if (chath && chath.trim() !== "") {
+        var chatt = document.getElementById("chat");
+        var chata = JSON.parse(chath);
+        chatt.innerHTML = ""; //Cleans the chat if there were any unwanted stuff
+        chata.forEach(function(message){
+           chatt.innerHTML += message;
+        });
+        chatt.innerHTML += "<center><h3>This is where your new conversation with MOONSTAR's 8ball begins.</h3></center>";
+        chatt.scrollTop = chatt.scrollHeight;
+    }
+}
+
+function choose(){
+    var choice = window.confirm("Choose an option:\n\n  - Change username.\n  - Clear chat history.")
+    if(choice){ // If user clicks accept
+        user="";
+        getperson();
+    }else{ // If user clicks cancel
+        localStorage.removeItem("chatHistory");
+        chatt = document.getElementById("chat");
+        chatt.innerHTML = "";
+        chatt.innerHTML += "<center><h3>This is where your chat with MOONSTAR's 8ball begins.</h3></center>";
+    }
 }
 
 function doFunction(){
@@ -36,8 +68,15 @@ function doFunction(){
     var chatt = document.getElementById('chat');
     if(mess.trim() != "" && mess != null){
         var randElement = responses[Math.floor(Math.random() * responses.length)];
-        document.getElementById("chat").innerHTML += "<br>" + person + ": " + mess;
-        setTimeout(function() {document.getElementById("chat").innerHTML += "<br>MOONSTAR: " + randElement; chatt.scrollTop = chatt.scrollHeight;}, 1000);
+        var message = "<br>" + person + ": " + mess
+        document.getElementById("chat").innerHTML += message;
+        saveMessage(message); //Saves the message to the local storage
+        setTimeout(function() {
+            var respd = "<br>MOONSTAR: " + randElement
+            document.getElementById("chat").innerHTML += respd;
+            saveMessage(respd);
+            chatt.scrollTop = chatt.scrollHeight;
+        }, 1000);
         chatt.scrollTop = chatt.scrollHeight;
         document.getElementById('message').value = "";
     }
@@ -50,3 +89,4 @@ function clickPress(event) {
 }
 
 getperson();
+displayHistory();
