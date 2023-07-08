574b1586e5a25c273866ffeb71cf803c30737546
Autumn <76168014+Autumn64@users.noreply.github.com>
2023-07-08 15:35:34 -0600

    Add files via upload

--- ball.js
+++ ball.js
@@ -14,21 +14,21 @@ function getperson(){
         }else{
             do{
                 person = prompt("Please enter a username.");
-            }while(person.trim() === "" || person === null);
-            var expiration_date = new Date();
+            }while(person.trim() === "");
+            let expiration_date = new Date();
             expiration_date.setYear (expiration_date.getFullYear () + 1);
             expiration_date = expiration_date.toGMTString();
-            var cookie_string = "username=" + person + "; path=/; expires=" + expiration_date;
+            let cookie_string = `username=${person}; path=/; expires=${expiration_date}`;
             document.cookie = cookie_string;
         }
     }catch(e){
-        getperson();
+        if (person === null) window.location.href = 'autumnslab.html';
     }
 }
 
 function saveMessage(message){ //Save the chat history
-    var chath = localStorage.getItem("chatHistory");
-    var chata = chath ? JSON.parse(chath) : []; //Checks if chath returned null, if so, it creates a blank array; if not, it parses it to JSON.
+    let chath = localStorage.getItem("chatHistory");
+    let chata = chath ? JSON.parse(chath) : []; //Checks if chath returned null, if so, it creates a blank array; if not, it parses it to JSON.
     chata.push(message);
     if(chata.length > 50){ //We can't store the whole goddamn chat history in the local storage
         chata.shift(); //Delete the oldest message
@@ -37,10 +37,10 @@ function saveMessage(message){ //Save the chat history
 }
 
 function displayHistory(){
-    var chath = localStorage.getItem("chatHistory");
+    let chath = localStorage.getItem("chatHistory");
     if (chath && chath.trim() !== "") {
-        var chatt = document.getElementById("chat");
-        var chata = JSON.parse(chath);
+        let chatt = document.getElementById("chat");
+        let chata = JSON.parse(chath);
         chatt.innerHTML = ""; //Cleans the chat if there were any unwanted stuff
         chata.forEach(function(message){
            chatt.innerHTML += message;
@@ -51,9 +51,10 @@ function displayHistory(){
 }
 
 function choose(){
-    var choice = window.confirm("Choose an option:\n\n  - Accept: Change username.\n  - Cancel: Clear chat history.")
+    let choice = window.confirm("Choose an option:\n\n  - Accept: Change username.\n  - Cancel: Clear chat history.")
     if(choice){ // If user clicks accept
         user="";
+        document.cookie = document.cookie = 'username=; path=/; expires=Thu, 01-Jan-70 00:00:01 GMT;';
         getperson();
     }else{ // If user clicks cancel
         localStorage.removeItem("chatHistory");
@@ -64,15 +65,15 @@ function choose(){
 }
 
 function doFunction(){
-    var mess = document.getElementById("message").value;
-    var chatt = document.getElementById('chat');
+    let mess = document.getElementById("message").value;
+    let chatt = document.getElementById('chat');
     if(mess.trim() !== "" && mess !== null){
-        var randElement = responses[Math.floor(Math.random() * responses.length)];
-        var message = "<br>" + person + ": " + mess
+        let randElement = responses[Math.floor(Math.random() * responses.length)];
+        let message = `<br>${person}: ${mess}`;
         document.getElementById("chat").innerHTML += message;
         saveMessage(message); //Saves the message to the local storage
         setTimeout(function() {
-            var respd = "<br>MOONSTAR: " + randElement
+            let respd = `<br>MOONSTAR: ${randElement}`;
             document.getElementById("chat").innerHTML += respd;
             saveMessage(respd);
             chatt.scrollTop = chatt.scrollHeight;
