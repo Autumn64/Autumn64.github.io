302350faea18b262acf433c2ff6b303fcf621c4b
Autumn64 <eunbyeol64@naver.com>
2023-12-01 23:34:15 -0600

    bruh

--- flag.js
+++ flag.js
@@ -13,7 +13,7 @@ var randflag = countries[Math.floor(Math.random() * countries.length)];
 
 let answers = [];
 
-for (let i = 0; i < 4; i++){
+for (let i = 0; i < 3; i++){
     let country = Math.floor(Math.random() * countries.length);
     if (!answers.includes(countries[country]) && country !== randflag){
         answers.push(countries[country]);
