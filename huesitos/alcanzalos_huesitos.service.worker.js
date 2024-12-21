a529c1669941807fcd2868b28f496e1456b12f79
Mónica Nayely Flores Gómez <autumn64@disroot.org>
2024-12-21 16:32:44 -0600

    Huesitos
    Signed-off-by: Mónica Nayely Flores Gómez <autumn64@disroot.org>

--- huesitos/alcanzalos_huesitos.service.worker.js
+++ huesitos/alcanzalos_huesitos.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734759004|213739115';
+const CACHE_VERSION = '1734820242|4824263850';
 /** @type {string} */
 const CACHE_PREFIX = 'Alcanzalos Huesi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
