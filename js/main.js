c5cf10b2eafdb1c5a7924f9ceb2f33da827efe0d
Mónica Nayely Flores Gómez <autumn64@disroot.org>
2025-09-06 01:07:39 -0600

    AGPLv3
    Signed-off-by: Mónica Nayely Flores Gómez <autumn64@disroot.org>

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+/*
+Autumn64's pages
+Copyright (C) 2023-2025 Mónica Gómez (Autumn64)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
+
 document.addEventListener("DOMContentLoaded", () =>{
     setMenuNavigation();
     setFooter();
@@ -34,3 +54,5 @@ const setFooter = () =>{
     </section>
     <p data-i18n="footer.p">Autumn64 &copy; 2023-2025. <a href="https://codeberg.org/Autumn64/pages" target="_blank">Source code</a></p>`;
 }
+
+// @license-end
